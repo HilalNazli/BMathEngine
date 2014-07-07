9ff0695314ce6ca5559fd12b3048ddf8859d43c6
Hilal Özdemir <hilal@Hilals-MacBook-Air.local>
2014-07-07 14:25:42 +0300

    Initial Commit

---
+++ BitesMathEngine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BitesMathEngine
+//
+//  Created by Hilal Özdemir on 07/07/14.
+//  Copyright (c) 2014 Hilal Özdemir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
